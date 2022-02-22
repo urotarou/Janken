0d3845103b38cb108cccb44e33b54b9eb0662232
urotarou <devak9kflab@gmail.com>
2022-02-22 20:29:11 +0900

    Initial Commit

---
+++ Janken/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Janken
+//
+//  Created by urotarou on 2022/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Janken/JankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenApp.swift
+//  Janken
+//
+//  Created by urotarou on 2022/02/22.
+//
+
+import SwiftUI
+
+@main
+struct JankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
